cde8e268045bc45c75bf6801fa8b4586861a16a2
375005009zd <375005009@qq.com>
2021-02-20 17:41:55 +0800

    新增contextPadProvider.js完全重构contextPad

--- src/components/contextPad/CustomContextPad.js
+++ src/components/contextPad/CustomContextPad.js
@@ -15,8 +15,9 @@ export default class CustomContextPad {
 
     contextPad.registerProvider(this);
   }
+}
 
-  getContextPadEntries(element) {
+ CustomContextPad.prototype.getContextPadEntries = function(element) {
     const {
       autoPlace,
       bpmnFactory,
@@ -24,72 +25,8 @@ export default class CustomContextPad {
       elementFactory,
       translate
     } = this;
-
-    function appendServiceTask(suitabilityScore) {
-      return function(event, element) {
-        if (autoPlace) {
-          const businessObject = bpmnFactory.create('bpmn:Task');
-    
-          businessObject.suitable = suitabilityScore;
-    
-          const shape = elementFactory.createShape({
-            type: 'bpmn:Task',
-            businessObject: businessObject
-          });
-    
-          autoPlace.append(element, shape);
-        } else {
-          appendServiceTaskStart(event, element);
-        }
-      }
-    }
-
-    function appendServiceTaskStart(suitabilityScore) {
-      return function(event) {
-        const businessObject = bpmnFactory.create('bpmn:Task');
-
-        businessObject.suitable = suitabilityScore;
-
-        const shape = elementFactory.createShape({
-          type: 'bpmn:Task',
-          businessObject: businessObject
-        });
-
-        create.start(event, shape, element);
-      }
-    }
-
-    return {
-      'create.low-task': {
-        group: 'model',
-        className: 'bpmn-icon-task red',
-        title: translate('Append Task with low suitability score'),
-        action: {
-          click: appendServiceTask(SUITABILITY_SCORE_LOW),
-          dragstart: appendServiceTaskStart(SUITABILITY_SCORE_LOW)
-        }
-      },
-      'append.average-task': {
-        group: 'model',
-        className: 'bpmn-icon-task yellow',
-        title: translate('Append Task with average suitability score'),
-        action: {
-          click: appendServiceTask(SUITABILITY_SCORE_AVERGE),
-          dragstart: appendServiceTaskStart(SUITABILITY_SCORE_AVERGE)
-        }
-      },
-      'append.high-task': {
-        group: 'model',
-        className: 'bpmn-icon-task green',
-        title: translate('Append Task with high suitability score'),
-        action: {
-          click: appendServiceTask(SUITABILITY_SCORE_HIGH),
-          dragstart: appendServiceTaskStart(SUITABILITY_SCORE_HIGH)
-        }
-      }
-    };
   }
-}
+
 
 CustomContextPad.$inject = [
   'bpmnFactory',
--- src/components/contextPad/index.js
+++ src/components/contextPad/index.js
@@ -1,6 +1,8 @@
-import CustomContextPad from './CustomContextPad'
-
+//import CustomContextPad from './CustomContextPad'
+import ContextPadProvider from './ContextPadProvider'
 export default {
-    __init__: ['customContextPad'],
-    customContextPad: ['type', CustomContextPad]
+    // __init__: ['customContextPad'],
+    // customContextPad: ['type', CustomContextPad]
+     __init__: ['contextPadProvider'],
+     contextPadProvider: ['type', ContextPadProvider]
 }
